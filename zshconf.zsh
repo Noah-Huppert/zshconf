014b42e5741f2007a782b89d284167bbc298d93d
Noah Huppert <noahuppert@gmail.com>
2016-06-04 16:33:08 -0400

    +parsing logic

--- zshconf.zsh
+++ zshconf.zsh
@@ -1,7 +1,23 @@
 ZSHCONF_FILE=${ZSHCONF_FILE:=$HOME/.config/zshconf/zshconf}
 
 if [[ -a $ZSHCONF_FILE ]]; then
-	cat $ZSHCONF_FILE
+	line_num=0
+	while read line; do
+		if [[ ! $line =~ "#" ]]; then 
+			if [[ $line =~ "[^#]+\.git" ]]; then
+				echo "Repo => $line"
+				repo_dir=$(echo $line | grep -Po "([^\/]+)\.git")
+				repo_dir=${repo_dir%\.git}
+			else
+				if [[ ! -z "$repo_dir" ]]; then
+					echo "File => { repo_dir: $repo_dir, line: $line }"
+				else
+					echo "[ERROR] Unexpected file path (Ignoring), expected git repository uri (line: $line_num)"
+				fi
+			fi
+		fi
+		((line_num++))
+	done <$ZSHCONF_FILE
 else
 	echo "No zshconf file found (Expected: $ZSHCONF_FILE)"
 fi
